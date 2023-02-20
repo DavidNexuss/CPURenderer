1fa0d2ce6f59a873b5cc5b508cc311a46e93de3f
DavidNexuss <davidgt7d1@gmail.com>
2023-02-20 09:44:53 +0100

    Fixes texture example

--- examples/texture.cpp
+++ examples/texture.cpp
@@ -3,9 +3,16 @@
 #include <thread>
 using namespace std;
 
+unsigned long int mnext = 1;
+
+int mrand(void) {
+  mnext = mnext * 1103515243 + 12345;
+  return (unsigned int)(mnext / 65536) % 32768;
+}
+
 int main() {
-  int width = 1920;
-  int height = 1080;
+  int width = 920 + 40;
+  int height = 764 + 40;
   ScreenBuffer buffer(width, height);
   PlayerConfiguration config;
   config.width = buffer.getScreenWidth();
@@ -16,10 +23,24 @@ int main() {
 
   ctx.writeTexture(20, 20, "../assets/sample.png");
 
-  player.uploadFrame();
+  auto t = std::thread([&]() {
+    while (true) {
+      int x = (mrand() % (width - 2)) + 1;
+      int y = (mrand() % (height - 2)) + 1;
+
+      int px = (mrand() % 3) - 1;
+      int py = (mrand() % 3) - 1;
+
+      std::swap(buffer[x][y], buffer[x + px][y + py]);
+    }
+  });
 
   while (!player.shouldClose()) {
+    if (player.isKeyJustPressed('E')) {
+      ctx.writeTexture(20, 20, "../assets/sample.png");
+    }
+    player.uploadFrame();
     player.drawFrame();
-    std::this_thread::sleep_for(50ms);
+    std::this_thread::sleep_for(16ms);
   }
 }
