e82012b881cc09d224b47bcb5a47886bcb1c90d8
DavidNexuss <davidgt7d1@gmail.com>
2023-02-20 09:44:53 +0100

    Fixes examples

--- examples/demo2.cpp
+++ examples/demo2.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <future>
 #include <iostream>
 #include <istream>
 #include <player.hpp>
@@ -15,10 +16,12 @@ string readFile(const char *path) {
   ss << ifs.rdbuf();
   return ss.str();
 }
+
+#define A(X) std::async(std::launch::async, [=]() { X });
 int main() {
 
-  int width = 1920 / 2;
-  int height = 1080 / 2;
+  int width = 1920;
+  int height = 1080;
 
   vector<color> screen(width * height);
   PlayerConfiguration configuration;
@@ -33,8 +36,16 @@ int main() {
 
   RenderContext ctx(player);
   ctx.setBrushColor({255, 100, 100});
-  ctx.setWriteCharDelay(11ms);
-  ctx.writeStr(20, height - 20, readFile("README.md").c_str());
+  ctx.setWriteCharDelay(1ms);
+
+  RenderContext a = ctx;
+  RenderContext b = ctx;
+  b.setBrushColor({255, 100, 255});
+
+  auto stop = A(a.writeStr(20, height - 20, readFile("README.md").c_str()););
+  std::this_thread::sleep_for(200ms);
+  auto stop2 =
+      A(b.writeStr(1000, height - 20, readFile("CMakeLists.txt").c_str()););
 
   std::this_thread::sleep_for(20000ms);
 }
